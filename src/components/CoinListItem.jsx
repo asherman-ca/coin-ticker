6facd0740d766a3d575dc95b94eee03a268ad47f
Alex Sherman <asherman.ca@gmail.com>
2022-05-12 12:18:58 -0700

    refetch progress

--- src/components/CoinListItem.jsx
+++ src/components/CoinListItem.jsx
@@ -2,6 +2,13 @@ import { Link } from 'react-router-dom';
 import '../styles/CoinListItem.css';
 
 const CoinListItem = ({ coin }) => {
+	let changeType = '';
+	if (coin.market_data.price_change_percentage_24h < -0.1) {
+		changeType = 'neg-change';
+	} else if (coin.market_data.price_change_percentage_24h > 0) {
+		changeType = 'pos-change';
+	}
+
 	return (
 		<tr className='coin-list-item'>
 			<td>
@@ -10,7 +17,7 @@ const CoinListItem = ({ coin }) => {
 				</Link>
 			</td>
 			<td>$ {coin.market_data.current_price.usd}</td>
-			<td>
+			<td className={changeType}>
 				{Math.round(coin.market_data.price_change_percentage_24h * 10) / 10}%
 			</td>
 			<td>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,26 +7,65 @@ const Home = () => {
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		fetch(`https://api.coingecko.com/api/v3/coins?per_page=20`)
-			.then((response) => response.json())
-			.then((response) => setCoins(response))
-			.then(() => setLoading(false));
+		const apiFetch = async () => {
+			const ref = await fetch(
+				`https://api.coingecko.com/api/v3/coins?per_page=20`
+			);
+			const response = await ref.json();
+			setCoins(response);
+			setLoading(false);
+		};
+		apiFetch();
 	}, []);
 
-	// const interval = setInterval(
-	// 	fetch(`https://api.coingecko.com/api/v3/coins?per_page=20`)
-	// 		.then((response) => response.json())
-	// 		.then((response) => console.log(response)),
-	// 	5000
-	// );
-
-	// const fetchData = () => {
-	// 	fetch(`https://api.coingecko.com/api/v3/coins?per_page=20`)
-	// 		.then((response) => response.json())
-	// 		.then((response) => setCoins(response));
+	// const apiReFetch = async () => {
+	// 	const ref = await fetch(
+	// 		`https://api.coingecko.com/api/v3/coins?per_page=20`
+	// 	);
+	// 	const response = await ref.json();
+	// 	console.log('res', response);
+	// 	setCoins(response);
 	// };
 
-	// setInterval(fetchData, 5000);
+	// setInterval(apiReFetch, 10000);
+
+	// const tickr = () => {
+	// 	setTick((prev) => {
+	// 		return tick + 1;
+	// 	});
+	// };
+
+	// setInterval(() => setTick(( + 1), 5000);
+
+	// console.log('hits');
+
+	// const liveCoinFetchr = async () => {
+	// 	console.log('hits');
+	// 	const thing = await fetch(
+	// 		new Request('https://api.livecoinwatch.com/coins/list'),
+	// 		{
+	// 			method: 'POST',
+	// 			headers: new Headers({
+	// 				'content-type': 'application/json',
+	// 				'x-api-key': '17e3d2ac-c2cf-4fb7-a839-fdf9fc65e6a9',
+	// 			}),
+	// 			body: JSON.stringify({
+	// 				current: 'USD',
+	// 				sort: 'rank',
+	// 				order: 'ascending',
+	// 				offset: 0,
+	// 				limit: 20,
+	// 				meta: false,
+	// 			}),
+	// 		}
+	// 	);
+	// 	const res = await thing.json();
+	// 	console.log('livewatchres', res);
+	// };
+
+	// liveCoinFetchr();
+
+	// setInterval(liveCoinFetchr, 3000);
 
 	if (loading) {
 		return <div>Loading</div>;
@@ -37,20 +76,25 @@ const Home = () => {
 			{console.log(coins)}
 			<div className='home'>
 				<div className='header'>
-					<span>Cryptocurrency Prices Live</span>
+					<span>Live Cryptocurrency Prices</span>
+
 					<span>Top Coins by Market Cap</span>
 				</div>
 				<div className='coin-list-container'>
 					<table className='coin-list'>
-						<tr>
-							<th>Coin</th>
-							<th>Price</th>
-							<th>% / 24hr</th>
-							<th>Market Cap</th>
-						</tr>
-						{coins.map((coin) => (
-							<CoinListItem coin={coin} key={coin.id} />
-						))}
+						<thead>
+							<tr>
+								<th>Coin</th>
+								<th>Price</th>
+								<th>% / 24hr</th>
+								<th>Market Cap</th>
+							</tr>
+						</thead>
+						<tbody>
+							{coins.map((coin) => (
+								<CoinListItem coin={coin} key={coin.id} />
+							))}
+						</tbody>
 					</table>
 				</div>
 			</div>
