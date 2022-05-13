0f4b9ae05000715048df088bdbef3c71a05891f5
Alex Sherman <asherman.ca@gmail.com>
2022-05-13 12:23:36 -0700

    coinview element refactor and tickers add

--- src/pages/CoinView.jsx
+++ src/pages/CoinView.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { capitalize } from '../utils/stringUtils';
+import {
+	capitalize,
+	changeDirection,
+	decimalReducer,
+} from '../utils/stringUtils';
 import '../styles/CoinView.css';
 
 const CoinView = () => {
@@ -28,53 +32,77 @@ const CoinView = () => {
 			</div>
 		);
 	}
+	{
+		console.log('coin', coin);
+	}
 
 	return (
 		<div className='container'>
 			<div className='coin-view'>
-				{console.log('coin', coin)}
 				<header>
 					<img src={coin.image.small} alt='logo' />
 					{capitalize(coin.id)} (#{coin.market_cap_rank})
 				</header>
 
-				<div className='details'>
-					<div className='row'>
-						<div className='title'>Price</div>
-						<div className='meta'>{coin.market_data.current_price.usd}</div>
-					</div>
-					<div className='row'>
-						<div className='title'>% / 24hr</div>
-						<div className='meta'>
-							{coin.market_data.price_change_percentage_24h}
+				<div className='detail-row'>
+					<div className='header'>Price Action</div>
+					<div className='list'>
+						<div className='col'>
+							<div className='title'>Price</div>
+							<div className='meta'>{coin.market_data.current_price.usd}</div>
 						</div>
-					</div>
-					<div className='row'>
-						<div className='title'>% / 7d</div>
-						<div className='meta'>
-							{coin.market_data.price_change_percentage_7d}
+						<div className='col'>
+							<div className='title'>/ 24hr</div>
+							<div
+								className={changeDirection(
+									coin.market_data.price_change_percentage_24h
+								)}
+							>
+								{decimalReducer(coin.market_data.price_change_percentage_24h)} %
+							</div>
 						</div>
-					</div>
+						<div className='col'>
+							<div className='title'>/ 7d</div>
+							<div
+								className={changeDirection(
+									coin.market_data.price_change_percentage_7d
+								)}
+							>
+								{decimalReducer(coin.market_data.price_change_percentage_7d)} %
+							</div>
+						</div>
 
-					<div className='row'>
-						<div className='title'>% / ATH</div>
-						<div className='meta'>
-							{coin.market_data.ath_change_percentage.usd}
-						</div>
-					</div>
-
-					<div className='row'>
-						<div className='title'>% / ATH</div>
-						<div className='meta'>
-							{coin.market_data.ath_change_percentage.usd}
+						<div className='col'>
+							<div className='title'>/ ATH</div>
+							<div
+								className={changeDirection(
+									coin.market_data.ath_change_percentage.usd
+								)}
+							>
+								{decimalReducer(coin.market_data.ath_change_percentage.usd)} %
+							</div>
 						</div>
 					</div>
 				</div>
 
-				<div className='description'>
-					<span>About:</span>
-					<span>{coin.description.en}</span>
+				<div className='ticker-row'>
+					<div className='header'>Tickers</div>
+					<div className='tickers'>
+						{coin.tickers.slice(0, 20).map((el) => (
+							<div className='col'>
+								<div className='title'>{el.market.name}</div>
+								<div className='meta'>{Math.trunc(el.last)}</div>
+							</div>
+						))}
+					</div>
 				</div>
+
+				{coin.description.en && (
+					<div className='description-row'>
+						<span>About</span>
+						{coin.description.en.replace(/<\/?a[^>]*>/g, '')}
+					</div>
+				)}
 			</div>
 		</div>
 	);
--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -2,4 +2,18 @@ const capitalize = (string) => {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-export { capitalize };
+const changeDirection = (changePercent) => {
+	if (changePercent < -0.08) {
+		return 'neg-change';
+	} else if (changePercent > 0.08) {
+		return 'pos-change';
+	} else {
+		return '';
+	}
+};
+
+const decimalReducer = (number) => {
+	return Math.round(number * 10) / 10;
+};
+
+export { capitalize, changeDirection, decimalReducer };
