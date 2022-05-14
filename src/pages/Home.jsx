aa81f8c87b2b91724a42874ce88e26d01f7f2144
Alex Sherman <asherman.ca@gmail.com>
2022-05-13 21:01:23 -0700

    tickers col text align change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,10 +43,10 @@ const Home = () => {
 
 				<div className='coin-list'>
 					<div className='coin-list-header'>
-						<div className='col link-col'>Asset</div>
+						<div className='col link-col'></div>
 						<div className='col'>Price</div>
 						<div className='col'>/ 24hr</div>
-						<div className='col'>Volume</div>
+						<div className='col'>Vol</div>
 						<div className='col'>M Cap</div>
 					</div>
 					{coins.map((coin) => (
