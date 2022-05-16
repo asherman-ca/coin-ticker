60afb7e59250ea239d56881479b7744d3bd014ca
Alex Sherman <asherman.ca@gmail.com>
2022-05-15 17:44:29 -0700

    coin list hover effect

--- src/components/CoinListItem.jsx
+++ src/components/CoinListItem.jsx
@@ -43,20 +43,6 @@ const CoinListItem = ({ coin }) => {
 				</span>
 			</div>
 		</div>
-		// <tr className='coin-list-item'>
-		// 	<td>
-		// 		<Link to={`/${coin.id}`} className='link'>
-		// 			<img src={coin.image.thumb} /> {capitalize(coin.id)}
-		// 		</Link>
-		// 	</td>
-		// 	<td>$ {coin.market_data.current_price.usd}</td>
-		// 	<td className={changeType}>
-		// 		{Math.round(coin.market_data.price_change_percentage_24h * 10) / 10}%
-		// 	</td>
-		// 	<td>
-		// 		{Math.round((coin.market_data.market_cap.usd / 1000000000) * 10) / 10}B
-		// 	</td>
-		// </tr>
 	);
 };
 
