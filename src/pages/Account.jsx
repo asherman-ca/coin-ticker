66879716c8517adcd3070c7a51c9ab9a40505495
Alex Sherman <asherman.ca@gmail.com>
2022-05-19 14:53:34 -0700

    pre transaction list refactor

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -16,6 +16,7 @@ import {
 	serverTimestamp,
 } from "firebase/firestore";
 import { toast } from "react-toastify";
+import { calcPNL } from "../utils/accounting";
 
 import "../styles/Account.css";
 import Spinner from "../components/Spinner";
@@ -123,94 +124,6 @@ const Account = () => {
 		toast.success("Order deleted");
 	};
 
-	const calcPNL = () => {
-		let accounts = {};
-		console.log(orders);
-
-		let buys = orders?.filter((order) => order.data.type === "buy");
-
-		let sells = orders?.filter((order) => order.data.type === "sell");
-
-		console.log("buys", buys);
-
-		buys?.forEach((order) => {
-			if (!accounts[order.data.coin]) {
-				accounts[order.data.coin] = {
-					coin: order.data.coin,
-					spent: order.data.spent,
-					total: order.data.spent / order.data.price,
-				};
-			} else {
-				accounts[order.data.coin].spent += order.data.spent;
-				accounts[order.data.coin].total += order.data.spent / order.data.price;
-			}
-		});
-
-		// let averages = {};
-
-		// Object.values(accounts).forEach((account) => {
-		// 	averages[account.coin] = {
-		// 		coin: account.coin,
-		// 		total: account.total,
-		// 		averagePrice: account.spent / account.total,
-		// 	};
-		// });
-
-		Object.values(accounts).forEach((account) => {
-			accounts[account.coin].averagePrice = account.spent / account.total;
-		});
-
-		sells?.forEach((order) => {
-			accounts[order.data.coin].total -= order.data.spent / order.data.price;
-			accounts[order.data.coin].spent -= order.data.spent;
-		});
-
-		console.log("accounts", accounts);
-
-		let PNL = [];
-
-		Object.values(accounts).forEach((account) => {
-			const currentPrice = coins.filter((coin) => coin.name === account.coin)[0]
-				.market_data.current_price.usd;
-
-			// PNL.push({
-			// 	coin: account.coin,
-			// 	pnl:
-			// 		account.spent * (currentPrice / account.averagePrice) - account.spent,
-			// });
-
-			console.log("spent", account.total * account.averagePrice);
-
-			console.log("profitability", currentPrice / account.averagePrice);
-
-			PNL.push({
-				coin: account.coin,
-				pnl:
-					account.total *
-						account.averagePrice *
-						(currentPrice / account.averagePrice) -
-					account.spent,
-				totalCoins: account.total,
-				averagePrice: account.averagePrice,
-			});
-		});
-
-		// Object.values(averages).forEach((av) => {
-		// 	const coinData = coins.filter((coin) => coin.name === av.coin);
-
-		// 	PNL.push({
-		// 		coin: av.coin,
-		// 		pnl:
-		// 			accounts[av.coin].spent *
-		// 				(coinData[0].market_data.current_price.usd /
-		// 					averages[av.coin].averagePrice) -
-		// 			accounts[av.coin].spent,
-		// 	});
-		// });
-
-		return PNL;
-	};
-
 	if (loading) {
 		return (
 			<div className='container'>
@@ -256,7 +169,7 @@ const Account = () => {
 							<div>Coins Held</div>
 							<div>Avg Price</div>
 						</div>
-						{calcPNL().map((el) => {
+						{calcPNL(orders, coins).map((el) => {
 							console.log(el);
 							return (
 								<div className='pnl-item'>
