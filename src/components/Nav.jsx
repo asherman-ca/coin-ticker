9120c25c5c9f514df07d6c46c77b236da03849bc
Alex Sherman <asherman.ca@gmail.com>
2022-05-24 11:43:36 -0700

    sign out button on nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,36 @@
-import { useState } from "react";
+import {
+	getAuth,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 import OAuth from "../components/OAuth";
 
 const Nav = () => {
-	const [param, setParam] = useState();
+	const auth = getAuth();
+
 	const navigate = useNavigate();
+	const [param, setParam] = useState();
+	const [loading, setLoading] = useState(true);
+	// const [currentUser, setCurrentUser] = useState(auth.currentUser);
+	const [loggedIn, setLoggedIn] = useState(false);
+	const isMounted = useRef(true);
+
+	useEffect(() => {
+		if (isMounted) {
+			const auth = getAuth();
+			onAuthStateChanged(auth, (user) => {
+				if (user) {
+					setLoggedIn(true);
+				} else {
+					setLoggedIn(false);
+				}
+			});
+		}
+	}, [isMounted]);
 
 	const onChange = (e) => {
 		e.preventDefault();
@@ -17,6 +41,41 @@ const Nav = () => {
 		e.preventDefault();
 		navigate(`/${param.toLowerCase()}`);
 	};
+
+	const onLogout = () => {
+		auth.signOut();
+		// setCurrentUser(null);
+		setLoggedIn(false);
+		navigate("/");
+		toast.info("Logged Out");
+	};
+
+	// console.log("current user", auth.currentUser);
+	// onAuthStateChanged(auth, (user) => {
+	// 	if (user) {
+	// 		// User is signed in, see docs for a list of available properties
+	// 		// https://firebase.google.com/docs/reference/js/firebase.User
+	// 		const uid = user.uid;
+	// 		console.log(uid);
+	// 		console.log("current user", currentUser);
+	// 		setCurrentUser(user);
+	// 		// ...
+	// 	} else {
+	// 		// User is signed out
+	// 		// ...
+	// 	}
+	// });
+
+	let authButton;
+	if (!loggedIn) {
+		authButton = <OAuth />;
+	} else {
+		authButton = (
+			<div className='sign-out-button' onClick={onLogout}>
+				Sign Out
+			</div>
+		);
+	}
 
 	return (
 		<div className='nav-container'>
@@ -35,7 +94,8 @@ const Nav = () => {
 						onClick={() => navigate("/account")}
 						className='fa-solid fa-circle-user'
 					></i>
-					<OAuth />
+					{authButton}
+					{/* <OAuth /> */}
 				</div>
 			</div>
 		</div>
