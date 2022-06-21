55ce90d981d413db77afa8ce62030b42d62fef50
Alex Sherman <asherman.ca@gmail.com>
2022-06-20 22:05:05 -0700

    cleanup

--- src/actions/NavActions.js
+++ src/actions/NavActions.js
@@ -1,12 +1,9 @@
 const filterSearchParam = (coins, searchParam) => {
-	console.log('search param filter', searchParam);
-	console.log('coins in filter', coins);
 	if (searchParam) {
 		return coins.filter((coin) =>
 			coin.name.toLowerCase().includes(searchParam.toLowerCase())
 		);
 	} else {
-		// return coins;
 		return [];
 	}
 };
