a246a60fdf68c80d6e3ada20c95cc8a762ad0f65
Alex Sherman <asherman.ca@gmail.com>
2022-06-22 11:59:08 -0700

    pagination v1 complete

--- src/pages/Home/components/Pagination.jsx
+++ src/pages/Home/components/Pagination.jsx
@@ -8,6 +8,13 @@ const Pagination = ({
 	rowsPerPage,
 	rowsPerPageOptions,
 }) => {
+	const lastPage =
+		totalCount % rowsPerPage == 0
+			? totalCount / rowsPerPage
+			: Math.floor(totalCount / rowsPerPage) + 1;
+
+	const paginationPages = paginationRange(currentPage, lastPage);
+
 	const nextPage = () => {
 		setCurrentPage(currentPage + 1);
 		window.scrollTo({
@@ -16,19 +23,13 @@ const Pagination = ({
 		});
 	};
 	const prevPage = () => {
+		console.log('current page', currentPage);
 		setCurrentPage(currentPage - 1);
 		window.scrollTo({
 			top: 0,
 			behavior: 'smooth',
 		});
 	};
-
-	const lastPage =
-		totalCount % rowsPerPage == 0
-			? totalCount / rowsPerPage
-			: Math.floor(totalCount / rowsPerPage) + 1;
-
-	const paginationPages = paginationRange(currentPage, lastPage);
 
 	const onSelect = (selectedValue) => {
 		setRowsPerPage(selectedValue);
@@ -38,37 +39,59 @@ const Pagination = ({
 		});
 	};
 
-	console.log('lastpage', lastPage);
-
 	return (
 		<div className='pagination'>
-			<button disabled={currentPage == 1} onClick={() => prevPage()}>
-				<i className='fa-solid fa-circle-chevron-left'></i>
-			</button>
+			<div
+				className='top-button'
+				onClick={() => {
+					window.scrollTo({
+						top: 0,
+						behavior: 'smooth',
+					});
+				}}
+			>
+				<i className='fa-solid fa-chevron-up'></i>
+			</div>
+			<div className='page-buttons'>
+				<button
+					disabled={currentPage == 1}
+					onClick={() => {
+						prevPage();
+					}}
+				>
+					<i className='fa-solid fa-chevron-left'></i>
+				</button>
 
-			{paginationPages.map((pageNumber) => {
-				return (
-					<div
-						className={
-							currentPage == pageNumber ? 'page-button active' : 'page-button'
-						}
-						onClick={() => {
-							setCurrentPage(pageNumber);
-							window.scrollTo({
-								top: 0,
-								behavior: 'smooth',
-							});
-						}}
-					>
-						{pageNumber}
-					</div>
-				);
-			})}
+				{paginationPages.map((pageNumber, ind) => {
+					if (pageNumber == '...') {
+						return <div key={ind}>...</div>;
+					} else {
+						return (
+							<div
+								key={ind}
+								className={
+									currentPage == pageNumber
+										? 'page-button active'
+										: 'page-button'
+								}
+								onClick={() => {
+									setCurrentPage(pageNumber);
+									window.scrollTo({
+										top: 0,
+										behavior: 'smooth',
+									});
+								}}
+							>
+								{pageNumber}
+							</div>
+						);
+					}
+				})}
 
-			<button disabled={currentPage == lastPage} onClick={() => nextPage()}>
-				<i className='fa-solid fa-circle-chevron-right' />
-			</button>
-
+				<button disabled={currentPage == lastPage} onClick={() => nextPage()}>
+					<i className='fa-solid fa-chevron-right'></i>
+				</button>
+			</div>
 			<select
 				name=''
 				id=''
