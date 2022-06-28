a2e5e066b9d707b7cd38ca07b1a440911cc6b733
Alex Sherman <asherman.ca@gmail.com>
2022-06-28 11:22:00 -0700

    signup styling

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GateButton from '../../components/GateButton';
+
+import { toast } from 'react-toastify';
 
 const SignUp = () => {
-	const onSubmit = (e) => {
-		e.preventDefault();
+	const [formData, setFormData] = useState({
+		email: '',
+		name: '',
+		password: '',
+		confirmPassword: '',
+	});
+
+	const onSubmit = () => {
+		if (Object.values(formData).some((el) => el == '')) {
+			toast.error('Complete all fields');
+		} else if (formData.password != formData.confirmPassword) {
+			toast.error('Passwords must match');
+		}
+		console.log('formData', formData);
 		console.log('hits');
+	};
+
+	const onChange = (e) => {
+		setFormData((prev) => {
+			return {
+				...prev,
+				[e.target.id]: e.target.value,
+			};
+		});
 	};
 
 	return (
@@ -12,10 +36,32 @@ const SignUp = () => {
 				<div className='auth-form-container'>
 					<div className='header'>Sign Up</div>
 					<div>to continue to Tickr</div>
-					<form className='auth-form' onSubmit={(e) => onSubmit(e)}>
-						<input type='text' />
-						<input type='text' />
-						<input type='text' />
+					<form className='auth-form'>
+						<input
+							onChange={onChange}
+							id='email'
+							type='email'
+							placeholder='Email'
+						/>
+						<input
+							id='name'
+							type='text'
+							placeholder='Name'
+							onChange={onChange}
+						/>
+						<input
+							id='password'
+							type='password'
+							placeholder='Password'
+							onChange={onChange}
+						/>
+						<input
+							id='confirmPassword'
+							type='password'
+							placeholder='Confirm Password'
+							onChange={onChange}
+						/>
+						<GateButton onClick={onSubmit}>Submit</GateButton>
 					</form>
 				</div>
 			</div>
