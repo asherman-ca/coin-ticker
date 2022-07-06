f6743499dbb9f8c938ac42a7053560510c1442e9
Alex Sherman <asherman.ca@gmail.com>
2022-07-06 14:25:23 -0700

    password reset validator

--- src/pages/Auth/PasswordReset.jsx
+++ src/pages/Auth/PasswordReset.jsx
@@ -6,15 +6,26 @@ import GateButton from '../../components/GateButton';
 
 const PasswordReset = () => {
 	const [email, setEmail] = useState();
+	const [errors, setErrors] = useState({ email: '' });
+
+	const validators = {
+		email: (string) => !string?.includes('@'),
+	};
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
-		try {
-			const auth = getAuth();
-			await sendPasswordResetEmail(auth, email);
-			toast.success('Email was sent');
-		} catch (error) {
-			toast.error('could not send reset email');
+		if (validators['email'](email)) {
+			setErrors((prev) => {
+				return { ...prev, email: 'Invalid Email Address' };
+			});
+		} else {
+			try {
+				const auth = getAuth();
+				await sendPasswordResetEmail(auth, email);
+				toast.success('Email was sent');
+			} catch (error) {
+				toast.error('Reset email error');
+			}
 		}
 	};
 
@@ -29,7 +40,12 @@ const PasswordReset = () => {
 					<div className='header'>Password Reset</div>
 					<div>Enter your email to recovery password</div>
 					<form onSubmit={onSubmit} className='auth-form'>
-						<input onChange={onChange} type='email' placeholder='Email' />
+						<div className='input-container'>
+							<input onChange={onChange} type='email' placeholder='Email' />
+							{errors?.email && (
+								<div className='form-error'>{errors.email}</div>
+							)}
+						</div>
 						<div className='button-row'>
 							<GateButton onClick={onSubmit} type={true}>
 								Submit
