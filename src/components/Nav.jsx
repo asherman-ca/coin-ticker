eff0e72b73e16d1d9990a35dafa1ea957757e825
Alex Sherman <asherman.ca@gmail.com>
2022-07-08 23:42:04 -0700

    navbar response

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -85,10 +85,7 @@ const Nav = ({ coinsLoading, coins }) => {
 	} else if (!loggedIn) {
 		authButton = (
 			<div className='nav-link'>
-				{/* Login */}
-				<div className='nav-link-button-container'>
-					<div className='nav-link-button'></div>
-				</div>
+				<i className='fa-solid fa-bars'></i>
 				<div className='nav-link-dropdown'>
 					<Link to={'/signin'}>
 						<i className='fa-solid fa-address-card'></i>Tickr Login
@@ -118,11 +115,7 @@ const Nav = ({ coinsLoading, coins }) => {
 		authButton = (
 			<>
 				<div className='nav-link'>
-					{/* {auth.currentUser.displayName} */}
-					{/* Account */}
-					<div className='nav-link-button-container'>
-						<div className='nav-link-button'></div>
-					</div>
+					<i className='fa-solid fa-bars' />
 					<div className='nav-link-dropdown'>
 						<div onClick={() => navigate('/account')}>
 							<i className='fa-solid fa-flask'></i>Testnet
