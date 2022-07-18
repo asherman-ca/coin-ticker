400fb728b28c9a09b3f5772ecfcecea9c34eb971
Alex Sherman <asherman.ca@gmail.com>
2022-07-18 15:28:56 -0700

    spacing progress

--- src/pages/Profile/components/LikedCoinItem.jsx
+++ src/pages/Profile/components/LikedCoinItem.jsx
@@ -4,9 +4,11 @@ const LikedCoinItem = ({ coin }) => {
 	console.log('like', coin);
 	return (
 		<div className='liked-coin-item'>
-			<div>{coin.id}</div>
-			<div>Body</div>
-			<div>footer</div>
+			<div className='content'>
+				<div>{coin.id}</div>
+				<div>Body</div>
+				<div>footer</div>
+			</div>
 		</div>
 	);
 };
