11256fde372af37b7945f22f8669c4e3cb0f6e04
Alex Sherman <asherman.ca@gmail.com>
2022-07-20 12:19:49 -0700

    liked item style clean

--- src/pages/Profile/components/LikedCoinItem.jsx
+++ src/pages/Profile/components/LikedCoinItem.jsx
@@ -12,7 +12,6 @@ const LikedCoinItem = ({ coin }) => {
 
 	return (
 		<Link to={`/coins/${coin.id}`} className='liked-coin-item'>
-			{console.log('coin', coin)}
 			<div className='content'>
 				<div className='title-col'>
 					<img src={coin.image.large} alt='' />
@@ -30,17 +29,6 @@ const LikedCoinItem = ({ coin }) => {
 						%
 					</div>
 				</div>
-				{/* <div className='name'>{coin.name}</div>
-				<img src={coin.image.large} alt='' />
-				<div className='price'>
-					<div>${cleanInt(coin.market_data.current_price.usd)}</div>
-					<div className={changeType}>
-						{coin.market_data.price_change_percentage_24h_in_currency.usd.toFixed(
-							2
-						)}
-						%
-					</div>
-				</div> */}
 			</div>
 		</Link>
 	);
