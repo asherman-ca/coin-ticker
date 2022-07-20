6268062419444e6b268b762efbe55cb4c86211e4
Alex Sherman <asherman.ca@gmail.com>
2022-07-20 12:13:23 -0700

    liked coin item fix

--- src/pages/Profile/components/LikedCoinItem.jsx
+++ src/pages/Profile/components/LikedCoinItem.jsx
@@ -12,8 +12,25 @@ const LikedCoinItem = ({ coin }) => {
 
 	return (
 		<Link to={`/coins/${coin.id}`} className='liked-coin-item'>
+			{console.log('coin', coin)}
 			<div className='content'>
-				<div className='name'>{coin.name}</div>
+				<div className='title-col'>
+					<img src={coin.image.large} alt='' />
+					<div>
+						<div className='liked-coin-name'>{coin.name}</div>
+						<div className='liked-coin-symbol'>{coin.symbol}</div>
+					</div>
+				</div>
+				<div className='meta-col'>
+					<div>${cleanInt(coin.market_data.current_price.usd)}</div>
+					<div className={changeType}>
+						{coin.market_data.price_change_percentage_24h_in_currency.usd.toFixed(
+							2
+						)}
+						%
+					</div>
+				</div>
+				{/* <div className='name'>{coin.name}</div>
 				<img src={coin.image.large} alt='' />
 				<div className='price'>
 					<div>${cleanInt(coin.market_data.current_price.usd)}</div>
@@ -23,7 +40,7 @@ const LikedCoinItem = ({ coin }) => {
 						)}
 						%
 					</div>
-				</div>
+				</div> */}
 			</div>
 		</Link>
 	);
